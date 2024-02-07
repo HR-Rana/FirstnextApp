644714e1ef4a546002e05abada6a1672edd1db65
webworld420 <76555387+webworld420@users.noreply.github.com>
2024-02-07 16:24:52 +0600

    done

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -12,6 +12,7 @@ import { IoLogOut } from "react-icons/io5";
 
 export default function Navbar() {
     const [openProfile, setOpenProfile] = useState(false);
+    const [menu, setMenu] = useState(false);
 
   return (
     <div className='navbar-area flex justify-between items-center px-5'>
@@ -40,9 +41,32 @@ export default function Navbar() {
                 </li>
                 </ul>
             </nav>
-            <div className="bars" onClick={(e)=>{setOpenProfile(!openProfile)}}>
                 {
-                    openProfile ? <RxCross1 /> :  <FaBars />
+                    menu ? <nav className="mobile-nav" style={{display:menu ?"block":"none" }}>
+                <ul className='flex justify-between'>
+                    <li><Link href={"/"}>Home</Link></li>
+                    <li><Link href={"/Login"}>Login</Link></li>
+                    <li><Link href={"/Registration"}>Registration</Link></li>
+                    <li  className="text-3xl cursor-pointer text-center text-black"
+                        onClick={(e)=>{setOpenProfile(!openProfile)}}
+                    >
+                    <FaCircleUser />
+                   {
+                    openProfile ?  <ul>
+                        <li><Link href="/Dashboard">Profile</Link></li>
+                        <li>Balance: <span>250Tk</span></li>
+                        <li><span><IoLogOut /></span> Logout</li>
+                    </ul> : null
+                   }
+                </li>
+                </ul>
+            </nav> :null
+                
+                }
+
+            <div className="bars" onClick={(e)=>{setMenu(!menu)}}>
+                {
+                    menu ? <RxCross1 /> :  <FaBars />
                 }
             </div>
         </div>
